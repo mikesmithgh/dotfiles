92b54628f504e39acbe728a5ebe9914bd33a7f37
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-04-04 13:21:02 -0400

    chore: update to nightly scripts

--- bash/scripts/nightly-kitty.sh
+++ bash/scripts/nightly-kitty.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 cd "$GOPATH/src/github.com/kovidgoyal/kitty" || exit 1
-git pull
+git fetch
+printf '\nUpdates: \n'
+git --no-pager log --oneline '...@{u}'
+printf '\npress enter to continue to continue or ctrl-c to abort\n'
+read -r
+git merge
+set -x
 ./dev.sh deps
 ./dev.sh build
--- bash/scripts/nightly-nvim.sh
+++ bash/scripts/nightly-nvim.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 cd "$HOME/gitrepos/neovim" || exit 1
-git pull
+git fetch
+printf '\nUpdates: \n'
+git --no-pager log --oneline '...@{u}'
+printf '\npress enter to continue to continue or ctrl-c to abort\n'
+read -r
+git merge
+set -x
 make distclean
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 nvim -c ":helptags ALL" -c "quit"
