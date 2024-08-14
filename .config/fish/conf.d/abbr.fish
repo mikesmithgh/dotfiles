fc18428010b9a855fb67b093f6dbb6aa25d38df3
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-08-14 09:34:42 -0400

    chore: sync some goodies

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -2,3 +2,5 @@ abbr --add vi nvim
 abbr --add vim nvim
 abbr --add k kubectl
 abbr --add g git
+abbr --add sprint "cd $HOME/neorg/notes; nvim (fd --max-depth 1 --type file 'sprint-\d+\.norg' | tail -1)"
+abbr --add notes "cd $HOME/neorg/notes; nvim +'Neorg index'"
---
+++ .config/fish/conf.d/config_sdk.fish
@@ -0,0 +1,1 @@
+set --global --export SDKMAN_DIR /opt/homebrew/opt/sdkman-cli/libexec
