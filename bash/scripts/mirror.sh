1bdcd6f0f5ecef579acd6d010f2d87f9663c4502
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-09-06 14:15:17 -0400

    fix: ignore failures during push in mirror script

--- bash/scripts/mirror.sh
+++ bash/scripts/mirror.sh
@@ -4,27 +4,27 @@ set -e -o pipefail
 git_push_args=("$*") # for example --dry-run
 
 mirror_repos=(
-	# public
-	'borderline.nvim'
-	'dotfiles'
-	'fzcf'
-	'git-prompt-string'
-	'git-prompt-string-lualine.nvim'
-	'gruvsquirrel.nvim'
-	'homebrew-git-prompt-string'
-	'kitty-scrollback.nvim'
-	'kitty-scrollback.nvim.wiki'
-	'kitty-scrollback.nvimconf'
-	'mikesmithgh'
-	'mikesmithgh.github.io'
-	'nvim'
-	'pdubs'
-	'render.nvim'
-	'render.nvim.wiki'
-	'ugbi'
-	'vimpromptu'
-	# private
-	'fonts'
+  # public
+  'borderline.nvim'
+  'dotfiles'
+  'fzcf'
+  'git-prompt-string'
+  'git-prompt-string-lualine.nvim'
+  'gruvsquirrel.nvim'
+  'homebrew-git-prompt-string'
+  'kitty-scrollback.nvim'
+  'kitty-scrollback.nvim.wiki'
+  'kitty-scrollback.nvimconf'
+  'mikesmithgh'
+  'mikesmithgh.github.io'
+  'nvim'
+  'pdubs'
+  'render.nvim'
+  'render.nvim.wiki'
+  'ugbi'
+  'vimpromptu'
+  # private
+  'fonts'
 )
 
 source='git@github.com:mikesmithgh'
@@ -35,23 +35,30 @@ mkdir -p "$mirror_dir"
 printf 'temporary mirror directory: %s\n' "$mirror_dir"
 
 for repo_name in "${mirror_repos[@]}"; do
-	cd "$mirror_dir" || exit 1
-	source_repo="$source/$repo_name.git"
-	printf 'cloning %s ...\n' "$source_repo"
-	git clone --mirror "$source_repo"
-	cd "$repo_name.git" || exit 1
+  cd "$mirror_dir" || exit 1
+  source_repo="$source/$repo_name.git"
+  printf 'cloning %s ...\n' "$source_repo"
+  git clone --mirror "$source_repo"
+  cd "$repo_name.git" || exit 1
 
-	for target in "${targets[@]}"; do
-		target_repo="$target/$repo_name.git"
-		git remote set-url --push origin "$target_repo"
-		printf 'mirroring %s to: %s\n' "$source_repo" "$target_repo"
-		printf 'git push --mirror %s\n' "${git_push_args[@]}"
-		if ((${#git_push_args[@]})); then
-			git push --mirror
-		else
-			git push --mirror "${git_push_args[@]}"
-		fi
-	done
+  for target in "${targets[@]}"; do
+    target_repo="$target/$repo_name.git"
+    git remote set-url --push origin "$target_repo"
+    printf 'mirroring %s to: %s\n' "$source_repo" "$target_repo"
+    printf 'git push --mirror %s\n' "${git_push_args[@]}"
+
+    # failures during push may be safe to ignore and we want to continue progressing for other remotes
+    # example: remote: Forgejo: The deletion of refs/pull/82/merge is skipped as it's an internal reference.
+    set +e
+
+    if ((${#git_push_args[@]})); then
+      git push --mirror
+    else
+      git push --mirror "${git_push_args[@]}"
+    fi
+
+    set -e
+  done
 
 done
 
