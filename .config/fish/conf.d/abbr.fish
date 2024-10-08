20c9195d7eb2dade967c1676611af8b7f65bbdd0
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-10-08 12:00:26 -0400

    chore: add kubectx abbr kx

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -2,6 +2,7 @@ abbr --add vi nvim
 abbr --add vim nvim
 abbr --add vimdiff nvim -d
 abbr --add k kubectl
+abbr --add kx kubectx
 abbr --add g git
 abbr --add sprint "cd $HOME/neorg/notes; nvim (fd --max-depth 1 --type file 'sprint-\d+\.norg' | tail -1)"
 abbr --add notes "cd $HOME/neorg/notes; nvim +'Neorg index'"
