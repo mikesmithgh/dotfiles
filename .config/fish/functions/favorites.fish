6866f3ac9414b75ae7aae0db845dc9d762a77267
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2024-12-15 16:30:46 -0500

    chore: formatting favorites.fish

--- .config/fish/functions/favorites.fish
+++ .config/fish/functions/favorites.fish
@@ -2,7 +2,16 @@ function favorites
     if test -n (commandline)
         commandline --function forward-char
     else
-        set --local dirs ~/ ~/repos/ ~/gitrepos/ ~/Pictures/screenshots/ ~/Pictures/screenshots/starstruck/ ~/neorg/notes/ectd-data/ ~/neorg/notes/haq-data/ ~/scripts/
+        set --local dirs \
+            ~/ \
+            ~/repos/ \
+            ~/gitrepos/ \
+            ~/Pictures/screenshots/ \
+            ~/Pictures/screenshots/starstruck/ \
+            ~/neorg/notes/ectd-data/ \
+            ~/neorg/notes/haq-data/ \
+            ~/scripts/
+
         set --local dir (printf '%s\n' $dirs | fzf)
         if test $status -ne 0
             commandline -a '
