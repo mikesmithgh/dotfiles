bac840fa9d34e325f732bb8b5e234b9bc2607d55
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2026-01-20 15:44:27 -0500

    chore: remove old fish abbreviations

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -4,7 +4,5 @@ abbr --add vimdiff nvim -d
 abbr --add k kubectl
 abbr --add kx kubectx
 abbr --add g git
-abbr --add sprint "cd $HOME/neorg/notes; nvim (fd --max-depth 1 --type file 'sprint-\d+\.norg' | tail -1)"
-abbr --add notes "cd $HOME/neorg/notes; nvim +'Neorg index'"
-abbr --add ectd "cd $HOME/neorg/notes; nvim eCTD-notes.norg"
+abbr --add notes "cd $HOME/neorg/notes"
 abbr --add s 'kitten ssh'
