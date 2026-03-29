255da101e71417e1639b47bf24044b279d098d1a
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2026-03-28 20:37:46 -0400

    chore(fish): add k9s write abbr

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -8,6 +8,7 @@ abbr --add vimdiff nvim -d
 abbr --add k kubectl
 abbr --add kx kubectx
 abbr --add g git
+abbr --add k9sw k9s --write
 
 abbr --add notes "cd $HOME/neorg/notes"
 abbr --add sprint "cd $HOME/neorg/notes; nvim (fd --max-depth 1 --type file 'sprint-.*\.norg' | tail -1)"
