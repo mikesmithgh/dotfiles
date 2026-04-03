7567e16df04fde635ad29247bd0d5f20c11ca0cd
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2026-04-03 10:35:14 -0400

    chore(nvim): add pinned flag to nightly script

--- bash/scripts/nightly-nvim.sh
+++ bash/scripts/nightly-nvim.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
+
 set -e
+
+pinned='4ed597389c33fe22911d1d8bad93173ec24920cb'
+
 cd "$HOME/gitrepos/neovim" || exit 1
-git fetch
-printf '\nUpdates: \n'
-git --no-pager log --oneline '...@{u}'
-printf '\npress enter to continue to continue or ctrl-c to abort\n'
-read -r
-git merge
-set -x
+
+if [[ "$1" == '--pinned' ]]; then
+  git checkout "$pinned"
+  ms_info "\nPinned at %s, press enter to continue to continue or ctrl-c to abort\n" "$pinned"
+  read -r
+else
+  git fetch
+  ms_info '\nUpdates: \n'
+  git --no-pager log --oneline '...@{u}'
+  ms_info '\npress enter to continue to continue or ctrl-c to abort\n'
+  read -r
+  git merge
+  set -x
+fi
+
 make distclean
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 nvim -c ":helptags ALL" -c "quit"
