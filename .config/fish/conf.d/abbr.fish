56e213d962d49521974a3d8bc7e0ad68e0a8e4a4
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2026-04-20 15:08:13 -0400

    chore(fish): add abbr for claude

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -6,10 +6,15 @@ abbr --add vi nvim
 abbr --add vim nvim
 abbr --add bnvim bobnvim
 abbr --add vimdiff nvim -d
+
 abbr --add k kubectl
 abbr --add kx kubectx
-abbr --add g git
 abbr --add k9sw k9s --write
+
+abbr --add g git
+
+abbr --add cl claude
+abbr --add clause claude
 
 abbr --add notes "cd $HOME/neorg/notes"
 abbr --add sprint "cd $HOME/neorg/notes; nvim (fd --max-depth 1 --type file 'sprint-.*\.norg' | tail -1)"
