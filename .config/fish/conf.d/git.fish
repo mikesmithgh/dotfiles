5192806d7ab98706431e244bcc4a3933f8e1e692
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2026-06-30 09:46:51 -0400

    fix(fish): move git completion to avoid shadowing

--- .config/fish/conf.d/git.fish
+++ .config/fish/conf.d/git.fish
@@ -1,3 +1,2 @@
-source $__fish_data_dir/completions/git.fish
-
+# git alias completions (dt = difftool). Lives in conf.d so it does NOT shadow fish 4.x's embedded git completions.
 complete --command git --condition '__fish_git_using_command dt diffview' --keep-order --exclusive --arguments '(complete --do-complete "git difftool ")'
